cec6f114c001472e7599f3d5c25aeb1392e4c90a
Morgan Teräs <72019791+DmgTaken@users.noreply.github.com>
2020-12-18 01:39:08 +0100

    Lots of changes

--- script.js
+++ script.js
@@ -2,41 +2,25 @@ function firstFunction() {
   document.getElementById("button1").style.backgroundColor = "#4285F4";
   document.getElementById("button2").style.backgroundColor = "#212529";
 	document.getElementById("button3").style.backgroundColor = "#212529";
-	document.getElementById("button4").style.backgroundColor = "#212529";
   document.getElementById("box1").style.display = ("block");
   document.getElementById("box2").style.display = ("none");
 	document.getElementById("box3").style.display = ("none");
-	document.getElementById("box4").style.display = ("none");
 }
 function secondFunction() {
   document.getElementById("button1").style.backgroundColor = "#212529";
   document.getElementById("button2").style.backgroundColor = "#4285F4";
 	document.getElementById("button3").style.backgroundColor = "#212529";
-	document.getElementById("button4").style.backgroundColor = "#212529";
   document.getElementById("box1").style.display = ("none");
   document.getElementById("box2").style.display = ("block");
 	document.getElementById("box3").style.display = ("none");
-	document.getElementById("box4").style.display = ("none");
 }
 function thirdFunction() {
   document.getElementById("button1").style.backgroundColor = "#212529";
   document.getElementById("button2").style.backgroundColor = "#212529";
 	document.getElementById("button3").style.backgroundColor = "#4285F4";
-	document.getElementById("button4").style.backgroundColor = "#212529";
   document.getElementById("box1").style.display = ("none");
   document.getElementById("box2").style.display = ("none");
 	document.getElementById("box3").style.display = ("block");
-	document.getElementById("box4").style.display = ("none");
-}
-function fourthFunction() {
-  document.getElementById("button1").style.backgroundColor = "#212529";
-  document.getElementById("button2").style.backgroundColor = "#212529";
-  document.getElementById("button3").style.backgroundColor = "#212529";
-  document.getElementById("button4").style.backgroundColor = "#4285F4";
-  document.getElementById("box1").style.display = ("none");
-  document.getElementById("box2").style.display = ("none");
-  document.getElementById("box3").style.display = ("none");
-  document.getElementById("box4").style.display = ("block");
 }
 function startTime() {
   var today = new Date();
@@ -45,8 +29,8 @@ function startTime() {
   var s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
-  document.getElementById('timetxt').innerHTML =
-  h + ":" + m + ":" + s;
+  document.getElementById('time').innerHTML =
+  ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2) + ":" + ("0" + s).slice(-2);
   var t = setTimeout(startTime, 500);
   }
   function checkTime(i) {
