ff14adb926b0e219d77be75dcd8d948f79057e5e
Morgan Teräs <72019791+DmgTaken@users.noreply.github.com>
2021-05-04 19:19:24 +0200

    Finished

--- script.js
+++ script.js
@@ -24,62 +24,6 @@ function startTime() {
     if (i < 10) {i = "0" + i};
     return i;
 }
-function authorDialog() {
-  var today = new Date();
-  alert("Morgan har skapat denna sida, året är " + today.getFullYear());
-}
-function textChange() {
-  document.getElementById("rubrik").innerHTML = "Morgan Teräs";
-  document.getElementById("text").style.backgroundColor = "#00FFEE";
-}
-function pictureChange() {
-  if (document.getElementById("picture").src == "https://upload.wikimedia.org/wikipedia/en/7/76/Darth_Vader.jpg") {
-    document.getElementById("picture").src = "https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28character%29.png";
-  }
-  else {
-    document.getElementById("picture").src = "https://upload.wikimedia.org/wikipedia/en/7/76/Darth_Vader.jpg";
-  }
-}
-function writeText() {
-  var fnamn = document.getElementById("fnamn").value;
-
-  var text = "Hej, " + fnamn + "!";
-  document.getElementById("demo").innerHTML = text;
-}
-function calculate() {
-  var tal1 = parseFloat(document.getElementById("tal1").value);
-  var tal2 = parseFloat(document.getElementById("tal2").value);
-  var text;
-  if (document.getElementById("type").value == "+") {
-    text = parseFloat(tal1 + tal2);
-  }
-  else if (document.getElementById("type").value == "-") {
-    text = parseFloat(tal1 - tal2);
-  }
-  else if (document.getElementById("type").value == "*") {
-    text = parseFloat(tal1 * tal2);
-  }
-  else if (document.getElementById("type").value == "/") {
-    text = parseFloat(tal1 / tal2);
-  }
-
-  document.getElementById("tal3").value = text;
-}
-function changeType() {
-  if (document.getElementById("type").value == "+") {
-    document.getElementById("type").value = "-";
-  }
-  else if (document.getElementById("type").value == "-") {
-    document.getElementById("type").value = "*";
-  }
-  else if (document.getElementById("type").value == "*") {
-    document.getElementById("type").value = "/";
-  }
-  else if (document.getElementById("type").value == "/") {
-    document.getElementById("type").value = "+";
-  }
-}
-
 function changeChoices() {
   var items;
 
